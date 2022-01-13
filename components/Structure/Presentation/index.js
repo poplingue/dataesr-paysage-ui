56ab54679176689c60c1b9a5223bb94ba73c3204
Jerem <jeremy.peglion@gmail.com>
2022-01-13 13:50:03 +0100

    init

--- components/Structure/Presentation/index.js
+++ components/Structure/Presentation/index.js
@@ -1,5 +1,7 @@
 import { Title } from '@dataesr/react-dsfr';
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
 import grid from '../../../helpers/imports';
+import NoSsrWrapper from '../../../helpers/no-ssr-wrapper';
 import { sectionUniqueId } from '../../../helpers/utils';
 import History from './History';
 
@@ -25,6 +27,29 @@ export default function Presentation({ content, section, expand }) {
                                     <Title as="h3" look="h6">
                                         {title}
                                     </Title>
+                                </Col>
+                            </Row>
+                            <Row spacing="px-2w">
+                                <Col spacing="pb-8w">
+                                    <NoSsrWrapper>
+                                        <MapContainer
+                                            center={[51.505, -0.09]}
+                                            zoom={13}
+                                            scrollWheelZoom={false}
+                                            style={{
+                                                height: 100,
+                                                width: '100%',
+                                            }}
+                                        >
+                                            <TileLayer
+                                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                            />
+                                            <Marker position={[51.505, -0.09]}>
+                                                <Popup>popip texte</Popup>
+                                            </Marker>
+                                        </MapContainer>
+                                    </NoSsrWrapper>
                                 </Col>
                             </Row>
                             <Row spacing="px-2w">
