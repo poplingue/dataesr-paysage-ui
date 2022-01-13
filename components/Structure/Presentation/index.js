47d72f8149b9d8358bbe40f15abbbeb3ade68317
Jerem <jeremy.peglion@gmail.com>
2022-01-13 15:05:59 +0100

    ajout du composant carte

--- components/Structure/Presentation/index.js
+++ components/Structure/Presentation/index.js
@@ -1,8 +1,7 @@
 import { Title } from '@dataesr/react-dsfr';
-import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
 import grid from '../../../helpers/imports';
-import NoSsrWrapper from '../../../helpers/no-ssr-wrapper';
 import { sectionUniqueId } from '../../../helpers/utils';
+import Map from '../../MapComponent';
 import History from './History';
 
 const components = {
@@ -11,8 +10,8 @@ const components = {
 
 export default function Presentation({ content, section, expand }) {
     const { Col, Row, Container } = grid();
-
-    return (
+    
+return (
         <>
             {content.map((subSection) => {
                 const { title, component } = subSection;
@@ -23,33 +22,15 @@ export default function Presentation({ content, section, expand }) {
                     <div key={title} data-section={dataSubSection}>
                         <Container fluid>
                             <Row spacing="px-2w">
+                                <Col spacing="pb-8w">
+                                    <Map />
+                                </Col>
+                            </Row>
+                            <Row spacing="px-2w">
                                 <Col>
                                     <Title as="h3" look="h6">
                                         {title}
                                     </Title>
-                                </Col>
-                            </Row>
-                            <Row spacing="px-2w">
-                                <Col spacing="pb-8w">
-                                    <NoSsrWrapper>
-                                        <MapContainer
-                                            center={[51.505, -0.09]}
-                                            zoom={13}
-                                            scrollWheelZoom={false}
-                                            style={{
-                                                height: 100,
-                                                width: '100%',
-                                            }}
-                                        >
-                                            <TileLayer
-                                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                            />
-                                            <Marker position={[51.505, -0.09]}>
-                                                <Popup>popip texte</Popup>
-                                            </Marker>
-                                        </MapContainer>
-                                    </NoSsrWrapper>
                                 </Col>
                             </Row>
                             <Row spacing="px-2w">
