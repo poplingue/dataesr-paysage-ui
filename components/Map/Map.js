2488e906ee5cd46d99d319570924efc888d5e7dc
Jerem <jeremy.peglion@gmail.com>
2022-01-21 10:42:49 +0100

    entete presentation avec map leaflet

---
+++ components/Map/Map.js
@@ -0,0 +1,55 @@
+import L from 'leaflet';
+import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+import * as ReactLeaflet from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const { MapContainer, TileLayer, Marker, Popup } = ReactLeaflet;
+
+const Map = ({ markers, height, width }) => {
+    useEffect(() => {
+        delete L.Icon.Default.prototype._getIconUrl;
+        L.Icon.Default.mergeOptions({
+            iconUrl:
+                'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+            shadowUrl:
+                'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        });
+    }, []);
+
+    const DEFAULT_CENTER = [48.866667, 2.333333];
+
+    return (
+        <MapContainer
+            center={DEFAULT_CENTER}
+            zoom={12}
+            style={{ height, width }}
+            scrollWheelZoom={false}
+            attributionControl
+        >
+            <TileLayer
+                attribution="<a href='https://www.jawg.io' target='_blank'>&copy; Jawg</a>"
+                url="https://tile.jawg.io/jawg-sunny/{z}/{x}/{y}.png?access-token=5V4ER9yrsLxoHQrAGQuYNu4yWqXNqKAM6iaX5D1LGpRNTBxvQL3enWXpxMQqTrY8"
+            />
+            {markers.map((marker) => (
+                <Marker position={marker.latLng} key={marker.latLng}>
+                    <Popup>{marker.address}</Popup>
+                </Marker>
+            ))}
+        </MapContainer>
+    );
+};
+
+export default Map;
+
+Map.defaultProps = {
+    markers: [{ latLng: [48.866667, 2.333333], address: 'Paris centre' }],
+    height: '300px',
+    width: '100%',
+};
+
+Map.propTypes = {
+    markers: PropTypes.array,
+    height: PropTypes.string,
+    width: PropTypes.string,
+};
--- components/Structure/Presentation/index.js
+++ components/Structure/Presentation/index.js
@@ -1,27 +1,15 @@
-import {
-    Button,
-    Callout,
-    CalloutText,
-    CalloutTitle,
-    Title,
-} from '@dataesr/react-dsfr';
-// import dynamic from 'next/dynamic';
+import { Title } from '@dataesr/react-dsfr';
 import grid from '../../../helpers/imports';
-import NoSsrWrapper from '../../../helpers/no-ssr-wrapper';
 import { sectionUniqueId } from '../../../helpers/utils';
-import CardInfo from '../../CardInfo';
-import Map from '../../MapComponent';
+import PresHeader from './Header';
 import History from './History';
 
-// const Map = dynamic(() => Promise.resolve('../../MapComponent'), {
-//   ssr: false
-// })
-
 const components = {
+    header: PresHeader,
     history: History,
 };
 
-export default function Presentation({ content, section, expand }) {
+export default function Presentation({ content, section }) {
     const { Col, Row, Container } = grid();
 
     return (
@@ -34,125 +22,6 @@ export default function Presentation({ content, section, expand }) {
                 return (
                     <div key={title} data-section={dataSubSection}>
                         <Container>
-                            {index === 0 ? (
-                                <>
-                                    <Row spacing="px-2w">
-                                        <Col spacing="pb-2w">
-                                            <NoSsrWrapper>
-                                                <Map />
-                                            </NoSsrWrapper>
-                                        </Col>
-                                    </Row>
-                                    <Row spacing="px-2w">
-                                        <Col spacing="pb-8w pr-1w">
-                                            <Callout
-                                                hasInfoIcon={false}
-                                                colorFamily="green-tilleul-verveine"
-                                            >
-                                                <CalloutTitle>
-                                                    Téléphone
-                                                </CalloutTitle>
-                                                <CalloutText size="md">
-                                                    +33 15 78 54 34 99
-                                                </CalloutText>
-                                            </Callout>
-                                        </Col>
-                                        <Col spacing="pb-8w pl-1w">
-                                            <Callout
-                                                hasInfoIcon={false}
-                                                colorFamily="green-tilleul-verveine"
-                                            >
-                                                <CalloutTitle>
-                                                    Adresse
-                                                </CalloutTitle>
-                                                <CalloutText size="md">
-                                                    5 allée Jacques Berque
-                                                    <br />
-                                                    44000 Nantes
-                                                </CalloutText>
-                                            </Callout>
-                                        </Col>
-                                    </Row>
-                                    <Row spacing="px-2w" gutters>
-                                        <Col n="4">
-                                            <CardInfo
-                                                // onClick={}
-                                                supInfo="date de création"
-                                                title="6 avril 2012"
-                                                actionLabel="Accéder au text officiel"
-                                            />
-                                        </Col>
-                                        <Col n="4">
-                                            <CardInfo
-                                                // onClick={}
-                                                supInfo="Tutelle"
-                                                title="Ministre chargé de l'enseignement supérieur"
-                                                actionLabel="Modifier"
-                                            />
-                                        </Col>
-                                        <Col n="4">
-                                            <CardInfo
-                                                // onClick={}
-                                                supInfo="Vague contractuelle"
-                                                title="Vague E"
-                                                actionLabel="Modifier"
-                                            />
-                                        </Col>
-                                        <Col n="4">
-                                            <CardInfo
-                                                // onClick={}
-                                                supInfo="Devise"
-                                                title="Vade retro satanas"
-                                                actionLabel="Modifier"
-                                            />
-                                        </Col>
-                                        <Col n="4">
-                                            <CardInfo
-                                                // onClick={}
-                                                supInfo="Statut juridique"
-                                                title="Association déclarée, reconnue d'utilité publique depuis 2008"
-                                                actionLabel="Modifier"
-                                            />
-                                        </Col>
-                                        <Col n="4">
-                                            <CardInfo
-                                                // onClick={}
-                                                supInfo="Logo"
-                                                image="https://upload.wikimedia.org/wikipedia/fr/c/cd/Logo_Sorbonne_Universit%C3%A9.png"
-                                                actionLabel="Modifier"
-                                            />
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
-                                            <Callout
-                                                hasInfoIcon={false}
-                                                colorFamily="blue-france"
-                                            >
-                                                <CalloutTitle>
-                                                    Contacts génériques
-                                                </CalloutTitle>
-                                                <CalloutText size="md">
-                                                    <ul>
-                                                        <li>Contact 1</li>
-                                                        <li>Contact 2</li>
-                                                        <li>Contact 3</li>
-                                                    </ul>
-                                                </CalloutText>
-                                                <div>
-                                                    <Button
-                                                        // size="sm"
-                                                        title="Voir tout"
-                                                        icon="ri-eye-2-line"
-                                                    >
-                                                        Voir tout
-                                                    </Button>
-                                                </div>
-                                            </Callout>
-                                        </Col>
-                                    </Row>
-                                </>
-                            ) : null}
                             <Row spacing="px-2w">
                                 <Col>
                                     <Title as="h3" look="h6">
