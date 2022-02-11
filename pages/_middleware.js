6e39c9683c42be34340e940e4debc28b878bbb34
paulinegc <paulinegc@protonmail.com>
2022-02-11 14:32:25 +0100

    middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,24 +1,41 @@
 import cookie from 'cookie';
 import { NextResponse } from 'next/server';
 
-const securedPaths = [
-    '/update',
-    '/account/activate-account',
-    '/search',
-    '/object',
+const okPaths = [
+    '/help',
+    '/resources',
+    '/sw',
+    '/fonts/Marianne-Regular.woff',
+    '/fonts/Marianne-Bold.woff2',
+    '/fonts/Marianne-Bold.woff',
+    '/fonts/Marianne-Regular.woff2',
+    '/api/auth/refresh-access-token',
+    '/favicon/favicon.ico',
+    '/api/auth/sign-in',
+    '/api/auth/signup',
+    '/api/user/me',
+    '/api/auth/refresh-access-token',
+    '/api/auth/send-password-renewal-code',
+    '/account/sign-in',
 ];
 
 export async function middleware(request) {
-    const regex = new RegExp('/[^/]+', 'g');
-    const matchPath = request.nextUrl.pathname.match(regex) || [];
-
     const headersCookies = request.headers.get('cookie');
     const cookies = cookie.parse(headersCookies ? headersCookies : '');
+    const currentPathName = request.nextUrl.pathname;
+    const pattern = okPaths.join('|');
+    let re = new RegExp(pattern, 'i');
 
+    // tokens | okPaths | home
     if (
-        securedPaths.indexOf(matchPath[0]) > -1 &&
-        !Object.keys(cookies).includes('tokens')
+        Object.keys(cookies).includes('tokens') ||
+        re.test(currentPathName) ||
+        currentPathName === '/'
     ) {
+        return NextResponse.next();
+    }
+
+    if (!Object.keys(cookies).includes('tokens') && !re.test(currentPathName)) {
         return NextResponse.redirect('/account/sign-in');
     }
 
